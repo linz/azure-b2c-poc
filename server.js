41cfb14b5c3bb32eac8da5b453267414984e6ad3
linzgmira <119823426+linzgmira@users.noreply.github.com>
2023-04-21 10:42:20 +1200

    add more user

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
+const { users } = require("./users");
 const fastify = require("fastify")({
   logger: true,
 });
 
-fastify.register(require('@fastify/formbody'))
+fastify.register(require("@fastify/formbody"));
 
-// fastify.get("/", async (request, reply) => {
-//   return { hello: "world" };
-// });
-
-fastify.get("/users/:user/:password", async (request, reply) => {  
+fastify.get("/users/:user/:password", async (request, reply) => {
   let user = "user1";
   let password = "password123";
   let res = {
-    validUserCode: "88888"
+    validUserCode: "88888",
   };
   if (user == request.params.user && password == request.params.password) {
     reply.status(200).send(res);
@@ -23,18 +20,24 @@ fastify.get("/users/:user/:password", async (request, reply) => {
       code: "errorCode",
       requestId: "requestId",
       userMessage: "Invalid user name and password.",
-      developerMessage: `The provided user ${request.params.user} and password cannot found.`
+      developerMessage: `The provided user ${request.params.user} and password cannot found.`,
     };
     reply.status(409).send(errorResponse);
   }
 });
 
-fastify.post("/users", async (request, reply) => {  
-  // console.log(`USER: ${request.body.user}`)
-  // console.log(`PASSWORD: ${request.body.password}`)
-  let user = "user1";
-  let password = "password123";
-  if (user == request.body.user && password == request.body.password) {
+fastify.post("/users", async (request, reply) => {
+  // console.log(`user: ${request.body.user}`);
+  // console.log(`pwd: ${request.body.password}`);
+
+  const found = users.find((obj) => {
+    return (
+      obj.user === request.body.user.toLowerCase() &&
+      obj.password === request.body.password
+    );
+  });
+
+  if (found) {
     reply.status(200).send();
   } else {
     let errorResponse = {
@@ -42,8 +45,8 @@ fastify.post("/users", async (request, reply) => {
       status: 409,
       code: "errorCode",
       requestId: "requestId",
-      userMessage: "Invalid user name and password.",
-      developerMessage: `The provided user ${request.params.user} and password cannot found.`
+      userMessage: "Invalid LINZ user name and password.",
+      developerMessage: `The provided user ${request.params.user} and password cannot found.`,
     };
     reply.status(409).send(errorResponse);
   }
---
+++ users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    user: "user1",
+    password: "password123",
+  },
+  {
+    user: "userx",
+    password: "password123x",
+  },
+  {
+    user: "usery",
+    password: "password123y",
+  },
+  {
+    user: "userz",
+    password: "password123z",
+  },
+];
+
+exports.users = users;
