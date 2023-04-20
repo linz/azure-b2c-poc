048f597f5a1dcd884bdd2ad3f059ab718087260b
linzgmira <119823426+linzgmira@users.noreply.github.com>
2023-04-21 11:06:53 +1200

    add names

--- users.js
+++ users.js
@@ -2,18 +2,26 @@ const users = [
   {
     user: "user1",
     password: "password123",
+    givenName: "Xyle",
+    surname: "Doe"
   },
   {
     user: "userx",
     password: "password123x",
+    givenName: "John",
+    surname: "Conner"
   },
   {
     user: "usery",
     password: "password123y",
+    givenName: "Ola",
+    surname: "Smith"
   },
   {
     user: "userz",
     password: "password123z",
+    givenName: "Juan",
+    surname: "Manuel"
   },
 ];
 
