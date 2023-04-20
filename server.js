f25b36491107df9018808bfb9318eacaf11aa7e1
linzgmira <119823426+linzgmira@users.noreply.github.com>
2023-04-20 20:22:43 +1200

    add post for testing

--- server.js
+++ server.js
@@ -27,6 +27,24 @@ fastify.get("/users/:user/:password", async (request, reply) => {
   }
 });
 
+fastify.post("/users/:user/:password", async (request, reply) => {  
+  let user = "user1";
+  let password = "password123";
+  if (user == request.params.user && password == request.params.password) {
+    reply.status(200).send();
+  } else {
+    let errorResponse = {
+      version: "1.0",
+      status: 409,
+      code: "errorCode",
+      requestId: "requestId",
+      userMessage: "Invalid user name and password.",
+      developerMessage: `The provided user ${request.params.user} and password cannot found.`
+    };
+    reply.status(409).send(errorResponse);
+  }
+});
+
 // Run the server!
 fastify.listen({ host: "::", port: 3000 }, function (err, address) {
   if (err) {
